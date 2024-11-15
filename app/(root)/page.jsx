b3dd57e1ac08bac1efbe1cb7683ae034c4dce3d5
santos-eric <esantosdev77@gmail.com>
2024-11-14 17:54:09 -0700

    added another container for the reviews section and title

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -2,16 +2,26 @@ import React from 'react';
 
 const Home = () => {
 	return (
-		<section className="blue_container">
-			<h1 className="heading">
-				Profit Lounge Kickstarts <br/> your resellng career
-			</h1>
-			<p className="sub-heading !max-w-3xl">
-				With cutting-edge tech that monitors every time a retailer changes a price, runs a promotion, or offers a discount
-			</p>
-			<p className="sub-heading !max-w-3xl">We help upgrade your side-hustle
-& offer so much more</p>
-		</section>
+		<>
+			<section className="blue_container">
+				<h1 className="heading">
+					Profit Lounge Kickstarts <br/> your resellng career
+				</h1>
+				<p className="sub-heading !max-w-3xl">
+					With cutting-edge tech that monitors every time a retailer changes a price, runs a promotion, or offers a discount
+				</p>
+				<p className="sub-heading !max-w-3xl">We help upgrade your side-hustle & offer so much more</p>
+			</section>
+			<section className="section_container">
+				<p className="tex-30-semibold">
+						Reviews
+				</p>
+
+				<ul className="mt-7 card_grid">
+					Review Card
+				</ul>
+			</section>
+		</>
 	)
 };
 
